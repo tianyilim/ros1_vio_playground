7aeabd5531fb1e61bce814eaf740e8eafc3bd8a2
Tianyi Lim <0.tianyi.lim@gmail.com>
2024-08-05 11:48:35 +0200

    update ros entrypoint for auto install

--- docker_build_utils/ros_entrypoint.sh
+++ docker_build_utils/ros_entrypoint.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
 set -e
 
+# This seems to be automatically executed by the ros:noetic image. It kinda sucks that it's not documented.
+
+# Build stuff
+cd / && ./post_create_command.sh
+
 # setup ros environment
 source "/opt/ros/$ROS_DISTRO/setup.bash"
 exec "$@"

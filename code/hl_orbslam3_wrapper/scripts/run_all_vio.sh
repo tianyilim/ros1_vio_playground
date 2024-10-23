eb6681f2a251f81f2c1fc4f085c321ec9559ffdd
tianyilim <0.tianyi.lim@gmail.com>
2024-10-23 18:12:43 +0200

    get rid of stuff

--- code/hl_orbslam3_wrapper/scripts/run_all_vio.sh
+++ code/hl_orbslam3_wrapper/scripts/run_all_vio.sh
@@ -5,8 +5,10 @@ source /catkin_ws/devel/setup.bash
 
 cd ../launch || exit
 
+rm -f /user/orbslam3_traj.tum
 roslaunch hilti22-orbslam-mono.launch rosbag_path:=$ROSBAG_PATH viz:=False && mv /user/orbslam3_traj.tum /user/orbslam3_mono.tum
 
+rm -f /user/orbslam3_traj.tum
 roslaunch hilti22-orbslam-stereo.launch rosbag_path:=$ROSBAG_PATH viz:=False && mv /user/orbslam3_traj.tum /user/orbslam3_stereo.tum
 
 roslaunch hilti22-vins-mono.launch rosbag_path:=$ROSBAG_PATH viz:=False && mv /user/vins-mono/output/vio.csv /user/vins_mono.tum

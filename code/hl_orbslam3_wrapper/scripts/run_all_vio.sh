4dcc6d8db8a5c377f7c7edd370f1a1aaa9b26ae6
tianyilim <0.tianyi.lim@gmail.com>
2024-10-23 17:07:51 +0200

    script to get all trajectories

---
+++ code/hl_orbslam3_wrapper/scripts/run_all_vio.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+ROSBAG_PATH="/mnt/ssd_2T/hilti-22/exp23_the_sheldonian_slam_part_0+.bag"
+
+cd ../launch || exit
+
+roslaunch hilti22-orbslam-mono.launch rosbag_path:=$ROSBAG_PATH viz:=False && mv /user/orbslam3_traj.tum /user/orbslam3_mono.tum
+
+roslaunch hilti22-orbslam-stereo.launch rosbag_path:=$ROSBAG_PATH viz:=False && mv /user/orbslam3_traj.tum /user/orbslam3_stereo.tum
+
+roslaunch hilti22-vins-mono.launch rosbag_path:=$ROSBAG_PATH viz:=False && mv /user/vins-mono/output/vio.csv /user/vins_mono.tum
+
+roslaunch hilti22-vins-stereo.launch rosbag_path:=$ROSBAG_PATH viz:=False && mv /user/vins-stereo/output/vio.csv /user/vins_stereo.tum

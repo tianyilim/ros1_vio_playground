dea1b1a59ea6647a1e3c90f42fe069a85dc901a5
tianyilim <0.tianyi.lim@gmail.com>
2024-10-27 20:15:51 +0100

    submodule update

--- code/hl_orbslam3_wrapper/scripts/run_all_vio.sh
+++ code/hl_orbslam3_wrapper/scripts/run_all_vio.sh
@@ -1,16 +1,34 @@
 #!/bin/bash
 
-ROSBAG_PATH="/mnt/ssd_2T/hilti-22/exp23_the_sheldonian_slam_part_0+.bag"
+ROSBAG_ROOT="/mnt/ssd_4T/tianyi_data/arche-long-indiv-bags"
+
+CAPTURES=(
+    "b3_to_b5_loop_round_field_traverse_2024-07-02_13.00.29_000.bag"
+    "b5_to_gp_to_b3_to_b4_traverse_2024-07-04_09.10.58_000.bag"
+    "b4_to_gp_to_d2_traverse_2024-07-04_09.39.45_000.bag"
+    "d2_to_gp_to_b5_2024-07-04_08.51.46_000.bag"
+    "b5_to_b3_big_traverse_2024-07-04_08.57.55_000.bag"
+)
+
 source /catkin_ws/devel/setup.bash
 
 cd ../launch || exit
 
-rm -f /user/orbslam3_traj.tum
-roslaunch hilti22-orbslam-mono.launch rosbag_path:=$ROSBAG_PATH viz:=False && mv /user/orbslam3_traj.tum /user/orbslam3_mono.tum
+for capture in "${CAPTURES[@]}"; do
+    echo "Processing capture: $capture"
 
-rm -f /user/orbslam3_traj.tum
-roslaunch hilti22-orbslam-stereo.launch rosbag_path:=$ROSBAG_PATH viz:=False && mv /user/orbslam3_traj.tum /user/orbslam3_stereo.tum
+    ls "$ROSBAG_ROOT/$capture"
+    ROSBAG_PATH="$ROSBAG_ROOT/$capture"
 
-roslaunch hilti22-vins-mono.launch rosbag_path:=$ROSBAG_PATH viz:=False && mv /user/vins-mono/output/vio.csv /user/vins_mono.tum
+    rm -f /user/orbslam3_traj.tum
+    roslaunch arche-orbslam-mono.launch rosbag_path:=$ROSBAG_PATH viz:=False
+    mv /user/orbslam3_traj.tum /user/orbslam3-$capture.tum
+    mv ~/.ros/KeyframeMemUsageKB.txt /user/orbslam3-"$capture"_memUsageKB.txt
+    mv ~/.ros/KeyframeTrackTiming.txt /user/orbslam3-"$capture"_timing.txt
 
-roslaunch hilti22-vins-stereo.launch rosbag_path:=$ROSBAG_PATH viz:=False && mv /user/vins-stereo/output/vio.csv /user/vins_stereo.tum
+    roslaunch arche-vins-mono.launch rosbag_path:=$ROSBAG_PATH viz:=False
+    mv /user/vins-mono/output/vio.csv /user/vins_mono.tum
+    mv ~/.ros/VINS_KeyframeMemUsageKB.txt /user/orbslam3-"$capture"_memUsageKB.txt
+    mv ~/.ros/VINS_KeyframeTrackTiming.txt /user/orbslam3-"$capture"_timing.txt
+
+done

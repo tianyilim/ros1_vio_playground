bc6a1adb47d0b893e1f0d6ca0e09799871cd8d40
tianyilim <0.tianyi.lim@gmail.com>
2024-11-01 18:13:51 +0100

    refine script to process openvins trajs, can also be used for okvis

--- code/hl_orbslam3_wrapper/scripts/process_openvins.py
+++ code/hl_orbslam3_wrapper/scripts/process_openvins.py
@@ -1,5 +1,14 @@
+import sys
+
 file_i = "/tmp/traj_estimate.txt"
 file_o = "/user/openvins_traj_estimate.tum"
+if len(sys.argv) > 1:
+    file_i = sys.argv[1]
+if len(sys.argv) > 2:
+    file_o = sys.argv[2]
+
+print("Input file: ", file_i)
+print("Output file: ", file_o)
 
 with open(file_i, 'r') as f:
     lines = f.readlines()
@@ -10,5 +19,20 @@ with open(file_o, 'w') as f:
             continue
 
         line = line.strip().split()
+        line = [line.strip(',') for line in line]
 
-        f.write(f"{line[0]} {line[1]} {line[2]} {line[3]} {line[4]} {line[5]} {line[6]} {line[7]}\n")
+        # Check if values are words
+        try:
+            timestamp = int(line[0])
+            timestamp = float(timestamp / 1e9)  # convert to seconds
+        except ValueError as e:
+            try:
+                timestamp = float(line[0])
+            except ValueError as e:
+                print("Skipping line: ", line)
+                continue
+
+        # print(line)
+        x, y, z, qx, qy, qz, qw, *_ = [float(x) for x in line[1:]]
+
+        f.write(f"{timestamp} {x} {y} {z} {qx} {qy} {qz} {qw}\n")

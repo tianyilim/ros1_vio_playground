096290ebd1cacb29df80f7ec5c11f4e31abede38
tianyilim <0.tianyi.lim@gmail.com>
2024-11-05 00:25:21 +0100

    rfactor

--- code/hl_orbslam3_wrapper/scripts/run_all_stereo_kitti.sh
+++ code/hl_orbslam3_wrapper/scripts/run_all_stereo_kitti.sh
@@ -10,49 +10,24 @@ function move_orbslam_outputs() {
     mv KeyframeTrackTiming.txt orbslam3_"$traj_name"_KeyframeTrackTiming.txt
 }
 
-./stereo_kitti ../../Vocabulary/ORBvoc.txt KITTI00-02.yaml /mnt/ssd_2T/kitti/dataset/sequences/00 0
-move_orbslam_outputs '00'
-./stereo_kitti ../../Vocabulary/ORBvoc.txt KITTI00-02.yaml /mnt/ssd_2T/kitti/dataset/sequences/01 0
-move_orbslam_outputs '01'
-./stereo_kitti ../../Vocabulary/ORBvoc.txt KITTI00-02.yaml /mnt/ssd_2T/kitti/dataset/sequences/02 0
-move_orbslam_outputs '02'
+for i in {00..02}; do
+    echo "Processing $i"
+    ./stereo_kitti ../../Vocabulary/ORBvoc.txt KITTI00-02.yaml /mnt/ssd_2T/kitti/dataset/sequences/"$i" 0
+    move_orbslam_outputs $i
+done
+
+echo "Processing $i"
 ./stereo_kitti ../../Vocabulary/ORBvoc.txt KITTI03.yaml /mnt/ssd_2T/kitti/dataset/sequences/03 0
-move_orbslam_outputs '03'
-./stereo_kitti ../../Vocabulary/ORBvoc.txt KITTI04-12.yaml /mnt/ssd_2T/kitti/dataset/sequences/04 0
-move_orbslam_outputs '04'
-./stereo_kitti ../../Vocabulary/ORBvoc.txt KITTI04-12.yaml /mnt/ssd_2T/kitti/dataset/sequences/05 0
-move_orbslam_outputs '05'
-./stereo_kitti ../../Vocabulary/ORBvoc.txt KITTI04-12.yaml /mnt/ssd_2T/kitti/dataset/sequences/06 0
-move_orbslam_outputs '06'
-./stereo_kitti ../../Vocabulary/ORBvoc.txt KITTI04-12.yaml /mnt/ssd_2T/kitti/dataset/sequences/07 0
-move_orbslam_outputs '07'
-./stereo_kitti ../../Vocabulary/ORBvoc.txt KITTI04-12.yaml /mnt/ssd_2T/kitti/dataset/sequences/08 0
-move_orbslam_outputs '08'
-./stereo_kitti ../../Vocabulary/ORBvoc.txt KITTI04-12.yaml /mnt/ssd_2T/kitti/dataset/sequences/09 0
-move_orbslam_outputs '09'
-./stereo_kitti ../../Vocabulary/ORBvoc.txt KITTI04-12.yaml /mnt/ssd_2T/kitti/dataset/sequences/00 0
-move_orbslam_outputs '00'
-./stereo_kitti ../../Vocabulary/ORBvoc.txt KITTI04-12.yaml /mnt/ssd_2T/kitti/dataset/sequences/00 0
-move_orbslam_outputs '00'
-./stereo_kitti ../../Vocabulary/ORBvoc.txt KITTI04-12.yaml /mnt/ssd_2T/kitti/dataset/sequences/11 0
-move_orbslam_outputs '11'
-./stereo_kitti ../../Vocabulary/ORBvoc.txt KITTI04-12.yaml /mnt/ssd_2T/kitti/dataset/sequences/12 0
-move_orbslam_outputs '12'
-./stereo_kitti ../../Vocabulary/ORBvoc.txt KITTI00-02.yaml /mnt/ssd_2T/kitti/dataset/sequences/13 0
-move_orbslam_outputs '13'
-./stereo_kitti ../../Vocabulary/ORBvoc.txt KITTI00-02.yaml /mnt/ssd_2T/kitti/dataset/sequences/14 0
-move_orbslam_outputs '14'
-./stereo_kitti ../../Vocabulary/ORBvoc.txt KITTI00-02.yaml /mnt/ssd_2T/kitti/dataset/sequences/15 0
-move_orbslam_outputs '15'
-./stereo_kitti ../../Vocabulary/ORBvoc.txt KITTI00-02.yaml /mnt/ssd_2T/kitti/dataset/sequences/16 0
-move_orbslam_outputs '16'
-./stereo_kitti ../../Vocabulary/ORBvoc.txt KITTI00-02.yaml /mnt/ssd_2T/kitti/dataset/sequences/17 0
-move_orbslam_outputs '17'
-./stereo_kitti ../../Vocabulary/ORBvoc.txt KITTI00-02.yaml /mnt/ssd_2T/kitti/dataset/sequences/18 0
-move_orbslam_outputs '18'
-./stereo_kitti ../../Vocabulary/ORBvoc.txt KITTI00-02.yaml /mnt/ssd_2T/kitti/dataset/sequences/19 0
-move_orbslam_outputs '19'
-./stereo_kitti ../../Vocabulary/ORBvoc.txt KITTI00-02.yaml /mnt/ssd_2T/kitti/dataset/sequences/20 0
-move_orbslam_outputs '20'
-./stereo_kitti ../../Vocabulary/ORBvoc.txt KITTI00-02.yaml /mnt/ssd_2T/kitti/dataset/sequences/21 0
-move_orbslam_outputs '21'
+move_orbslam_outputs 03
+
+for i in {04..12}; do
+    echo "Processing $i"
+    ./stereo_kitti ../../Vocabulary/ORBvoc.txt KITTI04-12.yaml /mnt/ssd_2T/kitti/dataset/sequences/"$i" 0
+    move_orbslam_outputs $i
+done
+
+for i in {13..21}; do
+    echo "Processing $i"
+    ./stereo_kitti ../../Vocabulary/ORBvoc.txt KITTI00-02.yaml /mnt/ssd_2T/kitti/dataset/sequences/"$i" 0
+    move_orbslam_outputs $i
+done

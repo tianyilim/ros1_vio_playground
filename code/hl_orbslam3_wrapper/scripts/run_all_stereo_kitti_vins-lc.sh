3ee2eb281f607523f520d1d9f8d9369cb527eafd
tianyilim <0.tianyi.lim@gmail.com>
2025-01-23 06:29:24 +0100

    VINS has hardcoded names???

--- code/hl_orbslam3_wrapper/scripts/run_all_stereo_kitti_vins-lc.sh
+++ code/hl_orbslam3_wrapper/scripts/run_all_stereo_kitti_vins-lc.sh
@@ -9,14 +9,15 @@ function move_vins_outputs() {
     local traj_name="kitti_$1"
     echo "Moving outputs for $traj_name"
     local OUTPUT_PATH=/user/vins-"$traj_name"
-    mkdir -p $OUTPUT_PATH
+    mkdir -p "$OUTPUT_PATH"
 
-    cp $VINS_OUTPUT_PATH/* $OUTPUT_PATH
+    cp $VINS_OUTPUT_PATH/* "$OUTPUT_PATH"
+    cp ~/.ros/pose_graph.txt "$OUTPUT_PATH"
     # sudo mv /tmp/VINS_KeyframeTrackTiming.txt /user/vins-stereo_"$traj_name"_KeyframeTrackTiming.txt
 }
 
-function run_vins(){
-    roslaunch hl_orbslam3_wrapper kitti-vins-stereo-lc.launch config_path:="$1" seq_path:="$2" 
+function run_vins() {
+    roslaunch hl_orbslam3_wrapper kitti-vins-stereo-lc.launch config_path:="$1" seq_path:="$2"
 }
 
 mkdir -p $VINS_OUTPUT_PATH
@@ -25,7 +26,7 @@ for i in {00..02}; do
     echo "Processing $i"
     run_vins "$VINS_CONFIG_PATH"/kitti_config00-02.yaml \
         /mnt/ssd_2T/kitti/dataset/sequences/"$i"
-    move_vins_outputs $i
+    move_vins_outputs "$i"
 done
 
 echo "Processing 03"
@@ -37,5 +38,5 @@ for i in {04..10}; do
     echo "Processing $i"
     run_vins $VINS_CONFIG_PATH/kitti_config04-12.yaml \
         /mnt/ssd_2T/kitti/dataset/sequences/"$i"
-    move_vins_outputs $i
+    move_vins_outputs "$i"
 done

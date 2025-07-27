fd5bcbebbaf597430f8adb61f6378338099bf288
tianyilim <0.tianyi.lim@gmail.com>
2025-07-27 04:29:52 +0200

    Unified VBR VINS run script

--- code/hl_orbslam3_wrapper/scripts/run_vins_vbr.sh
+++ code/hl_orbslam3_wrapper/scripts/run_vins_vbr.sh
@@ -1,34 +1,55 @@
 #!/bin/bash
 
-# set -e
+# TODO This is the main runner script for all VINS VBR datasets.
+set -e
 
-# Run KISS-ICP on all VBR datasets
-# Need to also save path to file
+# NOTE edit these as needed
+IMU_ON=false
+LC_ON=false
+
+if [ "$LC_ON" = true ]; then
+    echo "Loop Closure is ON"
+    LAUNCHFILE="vbr-vins-stereo-lc.launch"
+else
+    echo "Loop Closure is OFF"
+    LAUNCHFILE="vbr-vins-stereo.launch"
+fi
+
+if [ "$IMU_ON" = true ]; then
+    echo "IMU is ON"
+    CONFIG_PATH="/catkin_ws/src/hl_orbslam3_wrapper/cfg/vins-fusion/vbr-vins-stereo.yaml"
+else
+    echo "IMU is OFF"
+    CONFIG_PATH="/catkin_ws/src/hl_orbslam3_wrapper/cfg/vins-fusion/vbr-vins-stereo-no-imu.yaml"
+fi
 
 # List all train rosbags we have
-
+VINS_OUTPUT_PATH="/home/tony-ws1/output"
 BASE_PATH="/mnt/ssd_4T/tianyi_data/vbr/vbr_slam"
 ENVIRONMENTS=(
-    "campus"
-    "ciampino"
-    "colosseo"
-    "diag"
-    "pincio"
-    "spagna"
+    "colosseo colosseo_train0"
+    "campus campus_train0"
+    "campus campus_train1"
+    "ciampino ciampino_train0"
+    "ciampino ciampino_train1"
+    "diag diag_train0"
+    "pincio pincio_train0"
+    "spagna spagna_train0"
 )
 
-CONFIG_PATH="/catkin_ws/src/hl_orbslam3_wrapper/cfg/vins-fusion/vbr-vins-stereo.yaml"
+mkdir -p "$VINS_OUTPUT_PATH"
 
 for ENV in "${ENVIRONMENTS[@]}"; do
     # Iterate through subfolders
-    for SUBFOLDER in $(ls $BASE_PATH/$ENV); do
-        echo "Iterating through $ENV/$SUBFOLDER"
+    IFS=' ' read -r -a array <<<"$ENV"
+    SCENE=${array[0]}
+    SUBFOLDER=${array[1]}
 
-        ROSBAG_PATH=$BASE_PATH/$ENV/$SUBFOLDER/$SUBFOLDER.bag
+    ROSBAG_PATH=$BASE_PATH/$SCENE/$SUBFOLDER/$SUBFOLDER.bag
+    OUTPUT=/user/vins-"$SUBFOLDER"-"$LC_ON"-"$IMU_ON"
 
-        roslaunch hl_orbslam3_wrapper vbr-vins-stereo.launch viz:=False rosbag_path:="$ROSBAG_PATH" config_path:=$CONFIG_PATH
-        mv /user/vins-stereo/output/vio.csv /user/vins-stereo-"$SUBFOLDER".tum
-        mv ~/.ros/VINS_KeyframeMemUsageKB.txt /user/vins-stereo-"$SUBFOLDER"_memUsageKB.txt
-        mv ~/.ros/VINS_KeyframeTrackTiming.txt /user/vins-stereo-"$SUBFOLDER"_timing.txt
-    done
+    /usr/bin/time -o "$OUTPUT"/timing.txt roslaunch hl_orbslam3_wrapper "$LAUNCHFILE" rosbag_path:="$ROSBAG_PATH" config_path:="$CONFIG_PATH"
+
+    mkdir -p "$OUTPUT"
+    mv "$VINS_OUTPUT_PATH"/* "$OUTPUT"
 done
